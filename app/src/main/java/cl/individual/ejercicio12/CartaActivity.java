22785b7493527d4b66370a02df7922f660d59cb9
Emilia <correoalcantaraemilia@gmail.com>
2023-06-28 22:44:03 -0400

    Se agrega funcion al boton para ir a la segunda actividad + update de diseño + creacion de listView con el menu.

---
+++ app/src/main/java/cl/individual/ejercicio12/CartaActivity.java
@@ -0,0 +1,40 @@
+package cl.individual.ejercicio12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+import cl.individual.ejercicio12.databinding.ActivityCartaBinding;
+
+public class CartaActivity extends AppCompatActivity {
+private ActivityCartaBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCartaBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initList();
+    }
+
+    private void initList() {
+        ArrayList menu = new ArrayList<>();
+        menu.add("Salmón a la plancha con arroz y espárragos");
+        menu.add("Pollo teriyaki con sésamo");
+        menu.add("Solomillo de cordero al horno");
+        menu.add("Corvina asada con puré de papas");
+        menu.add("Brocheta de calabacín y pollo al limón");
+        menu.add("Ternera guisada con setas");
+        menu.add("Merluza con remolacha");
+        menu.add("Tacos de carne a la cerveza");
+        menu.add("Pasta de salmón a la mostaza");
+        menu.add("Hamburguesa vegana de alubia roja y quinoa");
+        menu.add("Lasaña de lenguado");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, menu);
+
+        binding.listViewMenu.setAdapter(adapter);
+    }
+}
--- app/src/main/java/cl/individual/ejercicio12/MainActivity.java
+++ app/src/main/java/cl/individual/ejercicio12/MainActivity.java
@@ -2,7 +2,9 @@ package cl.individual.ejercicio12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import cl.individual.ejercicio12.databinding.ActivityMainBinding;
 
@@ -14,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        initListeners();
+
     }
+
+    private void initListeners() {
+        binding.buttonMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CartaActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
 }
